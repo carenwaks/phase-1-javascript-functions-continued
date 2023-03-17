cccae622fc5fb576382b2f75505db73714fa9d12
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-03-17 17:19:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity= 'roller-skate') {
+    return (`This Saturday, I want to ${activity}!`)
+}
+saturdayFun();
+
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+};
+mondayWork();
+
+function wrapAdjective(params) {
+        
+}
+
+function wrapAdjective(result= "*") {
+    return function(emphatic = "special") {
+      return `You are ${result}${emphatic}${result}!`;
+    };
+  }
+console.log(wrapAdjective("%")("a dedicated programmer"));
